68a181921a0cb9887080001a401281bdbdf062d4
Matsaune123 <Mats.aune@hotmail.com>
2026-06-17 18:32:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// Serverer filene fra 'public'-mappen automatisk
-app.use(express.static('public'));
+// Serverer filene fra rota nå som 'public' er sletta
+app.use(express.static(__dirname));
 
 let players = {};
 
